56a8b3de436303810ca0d8a6ced48e614ec8dac3
PanabeeWQW <timofei.davydo@mail.ru>
2024-05-15 16:47:35 +0300

    Добавление редиректов + переопределение модели User

--- bus_station_test/settings.py
+++ bus_station_test/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'users.User'
 
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
